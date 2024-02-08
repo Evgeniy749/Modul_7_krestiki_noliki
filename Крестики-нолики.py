d0382b2205f09551ef958bad0463bc9f7381f0eb
Evgeniy749 <157996035+Evgeniy749@users.noreply.github.com>
2024-02-08 23:06:06 +0300

    Add files via upload

---
+++ Крестики-нолики.py
@@ -0,0 +1,71 @@
+# Крестики-нолики
+
+# количество клеток
+board_size = 3
+
+# игровое поле
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def draw_board():
+    ''' Выводим игровое поле'''
+    print('_' * 4 * board_size) # чертим горизонтальные линии в поле
+    for i in range(board_size):
+        print((' ' * 3 + '|') * 3) # чертим вертикальные линии в поле
+        print('', board[i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|' ) # вывод координат полей
+        print(('_' * 3 + '|') * 3)  # чертим вертикальные линии в поле
+
+def game_step(index, char):
+    '''Функция хода игрока'''
+    if (index > 9 or index < 1 or board[index - 1] in ('X', 'O')):
+        return False
+    board[index - 1] = char
+    return True
+
+
+def check_win():
+    '''Проверяем победу одного из играков'''
+    win = False
+
+    win_combination = (
+        (0,1,2), (3,4,5), (6,7,8),  # горизонтальные линии
+        (0,3,6), (1,4,7), (2,5,8),  # вертикальные линии
+        (0,4,8), (2,4,6)            # диагональные линии
+    )
+    for pos in win_combination:
+        if (board[pos[0]] == board[pos[1]] and board[pos[1]] == board[pos[2]]):
+            win = board[pos[0]]
+
+    return win
+
+def start_game():
+    current_player = 'X' # текущий игрок
+    step = 1 # номер шага
+    draw_board()
+
+    while (step < 10) and (check_win() == False): # С помощью цикла задаем количество ходов
+        index = input('Ходит игрок ' + current_player + '. Введите номер поля (0 - выход):')
+
+        if (index == '0'): # Принудительный выход из игры
+            break
+        #если получилось сделать шаг
+        if (game_step(int(index), current_player)):
+            print('Удачный ход')
+
+            if (current_player == 'X'):
+                current_player = 'O'
+            else:
+                current_player = 'X'
+
+            draw_board()
+            #увеличение номера хода
+            step += 1
+        else:
+            print('Неверный номер! Повторите!')
+
+    if (step == 10):
+        print('Игра окончена. Ничья!')
+    else:
+        print('Выиграл ' + check_win())
+
+print('Добро пожаловать в крестики-нолики!')
+start_game()
